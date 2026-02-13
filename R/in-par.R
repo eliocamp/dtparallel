63b424dcadd84c0ad4f81c05a7134468c859e5a1
Elio Campitelli <eliocampitelli@gmail.com>
2026-02-13 16:42:16 +1100

    Adds progress

--- R/in-par.R
+++ R/in-par.R
@@ -62,7 +62,7 @@ collect_par.tasks_future <- function(x) {
 
 #' @export
 collect_par.tasks_mirai <- function(x) {
-  unlist(mirai::collect_mirai(x))
+  unlist(mirai::collect_mirai(x, options = ".progress"))
 }
 
 #' @export
